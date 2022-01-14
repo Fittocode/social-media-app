c9d05b908e1ba4ace677e9eb9c666deb0aff3fc4
Matt Harrison <MattHarrison92@gmail.com>
2022-01-14 09:03:22 -0600

    add auth test

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
+const res = require('express/lib/response')
 
 // @route   POST api/users
 // @desc    Register user
@@ -49,9 +52,20 @@ async (req, res) => {
 
         // Return jsonwebtoken
 
-    console.log(req.body)
-    console.log(user.password)
-    res.send('User registered')
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'), 
+            { expiresIn: 36000 }, 
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token })
+        })
 
     } catch(err) {
         console.error(err.message)
--- server.js
+++ server.js
@@ -27,8 +27,6 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
-console.log('test')
 
 // app.get vs router.get 
 
-// 
